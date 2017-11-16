1f1639681923b70d19b51be0fbcd2eda59b18006
v0idum <akmal.melibaev@gmail.com>
2017-11-16 11:26:24 +0500

    PlayerTest - created

---
+++ test/com/aven/xo/model/PlayerTest.java
@@ -0,0 +1,31 @@
+package com.aven.xo.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    void getName() {
+        final String inputValue = "Akmal";
+        final String expectedValue = inputValue;
+        final Player player = new Player(inputValue, null);
+
+        final String actualValue = player.getName();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    void getFigure() {
+        final Figure inputValue = Figure.X;
+        final Figure expectedValue = inputValue;
+        final Player player = new Player(null, inputValue);
+
+        final Figure actualValue = player.getFigure();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+}
