1b125aaf228bbfbbcfeae138aac7432aab4c93bd
v0idum <akmal.melibaev@gmail.com>
2017-11-17 21:03:56 +0500

    FieldTest fixed (new Field(flexible fieldSize))

--- test/com/aven/xo/model/FieldTest.java
+++ test/com/aven/xo/model/FieldTest.java
@@ -1,6 +1,5 @@
 package com.aven.xo.model;
 
-import com.aven.xo.model.exceptions.AlreadyOccupiedException;
 import com.aven.xo.model.exceptions.InvalidPointException;
 import org.junit.jupiter.api.Test;
 
@@ -14,14 +13,14 @@ class FieldTest {
     public void getSize() throws Exception {
         final int expectedValue = 3;
 
-        final Field field = new Field();
+        final Field field = new Field(3);
 
         assertEquals(expectedValue, field.getSize());
     }
 
     @Test
     public void setFigure() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         Point point = new Point(0, 0);
         final Figure figure = Figure.X;
 
@@ -33,7 +32,7 @@ class FieldTest {
 
     @Test
     public void testGetFigureWhenFigureInNotSet() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point point = new Point(0, 0);
         final Figure figure = field.getFigure(point);
 
@@ -42,7 +41,7 @@ class FieldTest {
 
     @Test
     public void testGetFigureWhenXIsLessThanZero() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point point = new Point(-1, 0);
 
         try {
@@ -53,7 +52,7 @@ class FieldTest {
 
     @Test
     public void testGetFigureWhenYIsLessThanZero() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point point = new Point(1, -1);
 
         try {
@@ -64,7 +63,7 @@ class FieldTest {
 
     @Test
     public void testGetFigureWhenXAndYAreLessThanZero() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point point = new Point(-1, -1);
 
         try {
@@ -75,7 +74,7 @@ class FieldTest {
 
     @Test
     public void testGetFigureWhenXIsMoreThanMaxValue() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point point = new Point(field.getSize(), 0);
 
         try {
@@ -86,7 +85,7 @@ class FieldTest {
 
     @Test
     public void testGetFigureWhenYIsMoreThanMaxValue() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point point = new Point(1, field.getSize());
 
         try {
@@ -97,7 +96,7 @@ class FieldTest {
 
     @Test
     public void testGetFigureWhenXAndYAreMoreThanMaxValue() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point point = new Point(field.getSize(), field.getSize());
 
         try {
